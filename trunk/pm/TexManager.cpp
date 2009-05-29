a6f9b2dd7c4c22ce9d44e40a5c8333317adf5c54
fengyu05 <fengyu05@e74d88d6-4bf9-11de-b850-9977aa68d080>
2009-05-29 02:59:43 +0000

    add TexManager

---
+++ trunk/pm/TexManager.cpp
@@ -0,0 +1,10 @@
+#include "StdAfx.h"
+#include "TexManager.h"
+
+TexManager::TexManager(void)
+{
+}
+
+TexManager::~TexManager(void)
+{
+}
---
+++ trunk/pm/TexManager.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <hge.h>
+#include <lua.hpp>
+class TexManager
+{
+public:
+	TexManager(void);
+	~TexManager(void);
+};
