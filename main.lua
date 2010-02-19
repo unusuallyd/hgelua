199a72c82e869fbd6c2a43bcb9ff01f42aaa6c10
fengyu05 <fengyu05@e74d88d6-4bf9-11de-b850-9977aa68d080>
2010-02-19 07:43:34 +0000

    edit main

--- main.lua
+++ main.lua
@@ -1,83 +1,40 @@
 require("base")
+require("const")
 require("animation")
 
-local FPS = 24
-local SCREEN_WIDTH = 800
-local SCREEN_HEIGHT = 600
 
-local function FrameFunc()
-	local PressKey = hge.Input_GetKey()
-	if PressKey == HGEK_ESCAPE then
-		return true
-	end
-
-	if PressKey == HGEK_A then
-		anim:Play(2, HGEANIM_PINGPONG + HGEANIM_LOOP, 24)
-	end
-
-	if PressKey == HGEK_S then
-		anim:Play(3, HGEANIM_PINGPONG + HGEANIM_LOOP, 24)
-	end
-
-	if PressKey == HGEK_D then
-		anim:Play(4, HGEANIM_PINGPONG + HGEANIM_LOOP, 1)
-	end
-
-	if PressKey == HGEK_F then
-		anim:Play(1, HGEANIM_PINGPONG + HGEANIM_LOOP, 24)
-	end
-
-	if PressKey == HGEK_UP then
-		anim:Move(0, -5)
-	end
-	if PressKey == HGEK_DOWN then
-		anim:Move(0, 5)
-	end
-	if PressKey == HGEK_LEFT then
-		anim:Move(-5, 0)
-	end
-	if PressKey == HGEK_RIGHT then
-		anim:Move(5, 0)
-	end
-
-	anim:Update()
-	return false
-end
-
-
-local function RenderFunc()
-	hge.Gfx_Clear()
-	AnimationGroupRender()
-	font:Render(400, 300, HGETEXT_CENTER, "Hello HGE LUA")
-	return false
-end
 
 hge.System_SetState(HGE_WINDOWED, true);
-hge.System_SetState(HGE_FRAMEFUNC, FrameFunc);
-hge.System_SetState(HGE_RENDERFUNC, RenderFunc);
 hge.System_SetState(HGE_SCREENWIDTH, SCREEN_WIDTH);
 hge.System_SetState(HGE_SCREENHEIGHT, SCREEN_HEIGHT);
 hge.System_SetState(HGE_LOGFILE, "game.log");
 hge.System_SetState(HGE_SCREENBPP, 32);
 hge.System_SetState(HGE_FPS, FPS);
 
-hge.System_Init()
 
-anim = Animation:New()
+GameCtrl.Init()
 
-anim:Add(1, "Tex/ctrl_1.png", 5, 4, 1, 18)
-anim:Add(2, "Tex/ctrl_2.png", 5, 4, 1, 18)
-anim:Add(3, "Tex/ctrl_3.png", 5, 4, 1, 17)
-anim:Add(4, "Tex/ctrl_4.png", 5, 4, 1, 16)
-list = {1, 2, 3, 4}
 
-cb = AnimationList(anim, 24, list)
+local sec1 = 
+{
+	__dat = {},
+	InitFunc = function ()
+	end,
 
-anim:Play(1, 0, 24, cb)
+	EndFunc = function ()
+	end,
 
-font = hge.NewFont("Tex/font1.fnt")
-font:SetColor(RGB(255,0,0))
+	FrameFunc = function ()
+		return false
+	end,
 
-AddToAnimationGroup(anim, true)
+	RenderFunc = function ()	
+	end,
+}
 
-hge.System_MainLoop()
+GameCtrl.AddSection(1, sec1)
+GameCtrl.AddSection(2, sec2)
+GameCtrl.AddSection(3, sec3)
+
+GameCtrl.Start(1)
+
