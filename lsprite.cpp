6dff3329d2fd1a604e6ae85b293e4a60d32cc602
fengyu05 <fengyu05@e74d88d6-4bf9-11de-b850-9977aa68d080>
2010-02-21 06:28:18 +0000

    to sprite

--- lsprite.cpp
+++ lsprite.cpp
@@ -6,7 +6,7 @@
 
 #define HH2SPR(spr) ((hgeSprite*)(*spr))
 
-hgeSprite* ToAORS(lua_State *L, int index)
+hgeSprite* tosprite(lua_State *L, int index)
 {
 	lua_pushvalue(L, 1);	
 	lua_pushstring(L, "__name");
@@ -58,7 +58,7 @@ static int sprite_tostring(lua_State *L)
 
 static int sprite_render(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	if (lua_isnoneornil(L, 4))
 		spr->Render(
 				luaL_checknumber(L, 2),
@@ -77,7 +77,7 @@ static int sprite_render(lua_State *L)
 
 static int sprite_renderstretch(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->RenderStretch(
 			luaL_checknumber(L, 2),
 			luaL_checknumber(L, 3),
@@ -89,7 +89,7 @@ static int sprite_renderstretch(lua_State *L)
 
 static int sprite_render4v(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->Render4V(
 			luaL_checknumber(L, 2),
 			luaL_checknumber(L, 3),
@@ -105,7 +105,7 @@ static int sprite_render4v(lua_State *L)
 
 static int sprite_settexture(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	HHANDLE * tex = toudp(L, 2, TEXTURE);
 	spr->SetTexture(*tex);
 	return 0;
@@ -113,7 +113,7 @@ static int sprite_settexture(lua_State *L)
 
 static int sprite_settexturerect(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	bool adjSize = true;
 	if (!lua_isnoneornil(L, 6))
 		adjSize = lua_toboolean(L, 6);
@@ -129,7 +129,7 @@ static int sprite_settexturerect(lua_State *L)
 
 static int sprite_setcolor(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->SetColor(
 			luaL_checkinteger(L, 2),
 			luaL_optinteger(L, 3, -1)
@@ -139,7 +139,7 @@ static int sprite_setcolor(lua_State *L)
 
 static int sprite_setz(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->SetZ(
 			luaL_checkinteger(L, 2),
 			luaL_optinteger(L, 3, -1)
@@ -149,7 +149,7 @@ static int sprite_setz(lua_State *L)
 
 static int sprite_setblendmode(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->SetBlendMode(
 			luaL_checkinteger(L, 2)
 			);
@@ -158,7 +158,7 @@ static int sprite_setblendmode(lua_State *L)
 
 static int sprite_sethotspot(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->SetHotSpot(
 			luaL_checkinteger(L, 2),
 			luaL_checkinteger(L, 3)
@@ -168,7 +168,7 @@ static int sprite_sethotspot(lua_State *L)
 
 static int sprite_setflip(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	spr->SetFlip(
 			lua_toboolean(L, 2),
 			lua_toboolean(L, 3),
@@ -179,7 +179,7 @@ static int sprite_setflip(lua_State *L)
 
 static int sprite_gettexture(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	HHANDLE tex = spr->GetTexture();
 	HHANDLE * udp = newudp(L, TEXTURE);
 	(*udp) = tex;
@@ -188,7 +188,7 @@ static int sprite_gettexture(lua_State *L)
 
 static int sprite_gettexturerect(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	float x, y, w, h;
 	spr->GetTextureRect(&x, &y, &w, &h);
 	lua_pushnumber(L, x);
@@ -200,28 +200,28 @@ static int sprite_gettexturerect(lua_State *L)
 
 static int sprite_getcolor(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	lua_pushinteger(L, spr->GetColor(luaL_optinteger(L, 2, 0)));
 	return 1;
 }
 
 static int sprite_getz(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	lua_pushnumber(L, spr->GetZ(luaL_optinteger(L, 2, 0)));
 	return 1;
 }
 
 static int sprite_getblendmode(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	lua_pushinteger(L, spr->GetBlendMode());
 	return 1;
 }
 
 static int sprite_gethotspot(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	float x, y;
 	spr->GetHotSpot(&x, &y);
 	lua_pushnumber(L, x);
@@ -231,7 +231,7 @@ static int sprite_gethotspot(lua_State *L)
 
 static int sprite_getflip(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	bool bx, by;
 	spr->GetFlip(&bx, &by);
 	lua_pushboolean(L, bx);
@@ -241,21 +241,21 @@ static int sprite_getflip(lua_State *L)
 
 static int sprite_getwidth(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	lua_pushnumber(L, spr->GetWidth());
 	return 1;
 }
 
 static int sprite_getheight(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	lua_pushnumber(L, spr->GetHeight());
 	return 1;
 }
 
 static int sprite_getboundingbox(lua_State *L)
 {
-	hgeSprite * spr = ToAORS(L, 1);
+	hgeSprite * spr = tosprite(L, 1);
 	if (lua_gettop(L) == 4) // normal
 	{
 		hgeRect* rect = spr->GetBoundingBox(
